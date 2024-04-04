a272e702cb19a576d0ba2ce9a6db542b256f4e7a
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-04-04 13:27:02 +0200

    test

--- build.bat
+++ build.bat
@@ -1,2 +1,29 @@
 @echo off
-echo knowdont
+@echo.
+@echo ************************************************
+@echo DHCord builder/compiler by Daniel6745
+@echo ************************************************
+@echo.
+@echo Do you have Rust Toolchain, nodeJS and npm/pnpm?
+@echo.^<1^> Yes, i do.
+@echo.^<2^> No, give me links to install them.
+@echo.
+set /p CHOICE="Please select:"
+if "%CHOICE%" equ "Y" goto COMPILE_DHCORD
+if "%CHOICE%" equ "N" goto PRINT_LINKS
+:PRINT_LINKS
+) else (
+   @echo off
+   @echo.
+   @echo https://nodejs.org/en
+   @echo https://www.rust-lang.org/tools/install (use rustup!)
+   @echo After installing them, please re-run the batch.
+   @echo.
+:COMPILE_DHCORD
+) else (
+   @echo.
+   echo test
+   echo.
+   echo finished
+   exit
+)
